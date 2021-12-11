511aa230080806cfe66af18a5a58e59465e55996
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-12-12 08:30:11 +0900

    fix-message

--- IndiKadai6/ViewController.swift
+++ IndiKadai6/ViewController.swift
@@ -19,13 +19,13 @@ class ViewController: UIViewController {
     @IBAction private func judgementButton(_ sender: Any) {
         let answere = Int(answereSlider.value)
         guard (1...100).contains(answere) else {
-            showAlert("エラーが発生しました")
+            showAlert(message: "エラーが発生しました")
             return
         }
         if answere == randomInt {
-            showAlert("あたり！あなたの値：\(answere)")
+            showAlert(message: "あたり！あなたの値：\(answere)")
         } else {
-            showAlert("はずれ！あなたの値：\(answere)")
+            showAlert(message: "はずれ！あなたの値：\(answere)")
         }
     }
     private func reset() {
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         randomTextLabel.text = String(randomInt)
         answereSlider.value = 50
     }
-    private func showAlert(_ message: String) {
+    private func showAlert(message: String) {
         let alert = UIAlertController(title: "課題6", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "再挑戦", style: .default) { _ in
             self.reset()
